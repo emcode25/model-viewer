537bd3d21ecb55d367807aa22360539c91078b61
Evan Molinares <enhcode@gmail.com>
2025-05-17 01:08:44 -0400

    Added SDL, ImGui, and GLAD, as well as boilerplate code

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,109 @@
-﻿// model-viewer.cpp : Defines the entry point for the application.
-//
+﻿#include <glad/glad.h>
 
+#define SDL_MAIN_USE_CALLBACKS 1  /* use the callbacks instead of main() */
+#include <SDL3/SDL.h>
+#include <SDL3/SDL_main.h>
+#include <SDL3/SDL_opengl.h>
 #include <iostream>
 
-using namespace std;
+#include <imgui.h>
+#include <imgui_impl_sdl3.h>
+#include <imgui_impl_opengl3.h>
 
-int main()
+static SDL_Window* window = NULL;
+static SDL_GLContext ctx = {};
+
+/* This function runs once at startup. */
+SDL_AppResult SDL_AppInit(void** appstate, int argc, char* argv[])
 {
-	cout << "Hello CMake." << endl;
-	return 0;
+    //Make info visible for all categories
+    SDL_SetHint(SDL_HINT_LOGGING, "app=info,assert=warn,test=verbose,*=info");
+
+    //Set OpenGL 4.6
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 4);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 6);
+
+    //Create window and context
+    window = SDL_CreateWindow("Model Viewer", 800, 600, SDL_WINDOW_OPENGL);
+    ctx = SDL_GL_CreateContext(window);
+
+    //Initialize GLAD
+    if (!gladLoadGLLoader((GLADloadproc)SDL_GL_GetProcAddress))
+    {
+        SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, "[INIT] GLAD failed to initialize.");
+        return SDL_APP_FAILURE;
+    }
+    else
+    {
+        SDL_LogInfo(SDL_LOG_CATEGORY_APPLICATION, "[INIT] GLAD initialized successfully.");
+    }
+
+    //Set OpenGL options
+    glClearColor(0.2f, 0.2f, 0.2f, 1.0f);
+    glEnable(GL_DEPTH_TEST);
+
+    //Start Dear ImGui
+    IMGUI_CHECKVERSION();
+    ImGui::CreateContext();
+    ImGuiIO& io = ImGui::GetIO();
+    io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;
+
+    //Set up backend links
+    ImGui_ImplSDL3_InitForOpenGL(window, ctx);
+    ImGui_ImplOpenGL3_Init();
+
+    SDL_LogInfo(SDL_LOG_CATEGORY_APPLICATION, "[INIT] Model Viewer initialized successfully.");
+    return SDL_APP_CONTINUE;
+}
+
+/* This function runs when a new event (mouse input, keypresses, etc) occurs. */
+SDL_AppResult SDL_AppEvent(void* appstate, SDL_Event* event)
+{
+    //Let Dear ImGui see what happened too
+    ImGui_ImplSDL3_ProcessEvent(event);
+
+    //Look through keypresses
+    if (event->type == SDL_EVENT_KEY_DOWN) {
+        switch (event->key.key)
+        {
+            case SDLK_ESCAPE:
+                return SDL_APP_SUCCESS;
+            default:
+                break;
+        }
+    }
+
+    //Check if the user asked to quit
+    if (event->type == SDL_EVENT_QUIT)
+        return SDL_APP_SUCCESS;
+
+    return SDL_APP_CONTINUE;
+}
+
+/* This function runs once per frame, and is the heart of the program. */
+SDL_AppResult SDL_AppIterate(void* appstate)
+{
+    ImGui_ImplOpenGL3_NewFrame();
+    ImGui_ImplSDL3_NewFrame();
+    ImGui::NewFrame();
+
+    //ImGui::ShowDemoWindow();
+
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+    ImGui::Render();
+    ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+
+    SDL_GL_SwapWindow(window);
+    return SDL_APP_CONTINUE;
+}
+
+/* This function runs once at shutdown. */
+void SDL_AppQuit(void* appstate, SDL_AppResult result)
+{
+    ImGui_ImplOpenGL3_Shutdown();
+    ImGui_ImplSDL3_Shutdown();
+    ImGui::DestroyContext();
+    SDL_GL_DestroyContext(ctx);
+    SDL_DestroyWindow(window);
 }
